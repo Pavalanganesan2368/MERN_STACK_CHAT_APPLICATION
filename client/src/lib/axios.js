6ef08b83773d3f304993fdc91139d3de654b9613
Pavalan G <136297958+Pavalanganesan2368@users.noreply.github.com>
2026-03-26 11:13:23 +0530

    Project Completed

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://mern-stack-chat-application-wntn.onrender.com/api" : "http://localhost:5001/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://mern-stack-chat-application-wntn.onrender.com" : "http://localhost:5001/api",
   withCredentials: true,
 });
